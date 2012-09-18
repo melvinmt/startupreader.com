ad525226701567adb5620b7826a8c2200d4f4038
Melvin Tercan <mmtercan@gmail.com>
2012-09-18 01:33:31 -0700

    store fetched data in datastore

--- app/controller/controller.go
+++ app/controller/controller.go
@@ -9,6 +9,8 @@ import (
 	"app/helper/tpl"
 	"app/helper/url"
 	"app/model"
+	"appengine"
+	"appengine/datastore"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -19,6 +21,8 @@ func Index(w http.ResponseWriter, r *http.Request) {
 }
 
 func FetchStartups(w http.ResponseWriter, r *http.Request) {
+
+	c := appengine.NewContext(r)
 
 	// href := "http://api.crunchbase.com/v/1/companies.js"
 	href := "http://f.cl.ly/items/1L3V1b0v453C133v1n1L/companies.js"
@@ -36,7 +40,18 @@ func FetchStartups(w http.ResponseWriter, r *http.Request) {
 
 	// loop through startups
 	for _, startup := range startups {
-		fmt.Fprintf(w, "Name: %s\nPermalink: %s\n\n", startup.Name, startup.Permalink)
+
+		q, _ := datastore.NewQuery("Startup").KeysOnly().Limit(1).Filter("Permalink=", startup.Permalink).Count(c)
+		if q > 0 {
+			fmt.Fprintf(w, "%s (%s) has already been saved.\n", startup.Name, startup.Permalink)
+			continue
+		}
+		_, err := datastore.Put(c, datastore.NewKey(c, "Startup", startup.Permalink, 0, nil), &startup)
+		if err != nil {
+			fmt.Fprintf(w, "%s (%s) could not been saved.\n", startup.Name, startup.Permalink)
+			continue
+		}
+		fmt.Fprintf(w, "%s (%s) has been saved.\n", startup.Name, startup.Permalink)
 	}
 
 }
